810da7bdc4d77b7c80a58918449d96b5f19a13ab
jaxs-ribs <luc.baracat@googlemail.com>
2025-03-28 16:35:07 +0100

    Regenerate stubs

--- caller-utils/src/lib.rs
+++ caller-utils/src/lib.rs
@@ -12,6 +12,37 @@ pub use hyperware_app_common::send;
 use hyperware_process_lib::Address;
 use serde_json::json;
 
+/// Generated RPC stubs for the curator interface
+pub mod curator {
+    use crate::*;
+
+    /// Generated stub for `add-curation` remote RPC call
+    pub async fn add_curation_remote_rpc(target: &Address, title: String, content: String) -> SendResult<bool> {
+        let request = json!({"AddCuration": (title, content)});
+        send::<bool>(&request, target, 30).await
+    }
+    
+    /// Generated stub for `remove-curation` remote RPC call
+    pub async fn remove_curation_remote_rpc(target: &Address, title: String) -> SendResult<bool> {
+        let request = json!({"RemoveCuration": title});
+        send::<bool>(&request, target, 30).await
+    }
+    
+    /// Generated stub for `get-curation` remote RPC call
+    pub async fn get_curation_remote_rpc(target: &Address, title: String) -> SendResult<Option<String>> {
+        let request = json!({"GetCuration": title});
+        send::<Option<String>>(&request, target, 30).await
+    }
+    
+    /// Generated stub for `temp` remote RPC call
+    pub async fn temp_remote_rpc(target: &Address) -> SendResult<f32> {
+        let request = json!({"Temp" : {}});
+        send::<f32>(&request, target, 30).await
+    }
+    
+    
+}
+
 /// Generated RPC stubs for the indexer interface
 pub mod indexer {
     use crate::*;
@@ -63,37 +94,6 @@ pub mod client {
     pub async fn just_leet_remote_rpc(target: &Address) -> SendResult<u32> {
         let request = json!({"JustLeet" : {}});
         send::<u32>(&request, target, 30).await
-    }
-    
-    
-}
-
-/// Generated RPC stubs for the curator interface
-pub mod curator {
-    use crate::*;
-
-    /// Generated stub for `add-curation` remote RPC call
-    pub async fn add_curation_remote_rpc(target: &Address, title: String, content: String) -> SendResult<bool> {
-        let request = json!({"AddCuration": (title, content)});
-        send::<bool>(&request, target, 30).await
-    }
-    
-    /// Generated stub for `remove-curation` remote RPC call
-    pub async fn remove_curation_remote_rpc(target: &Address, title: String) -> SendResult<bool> {
-        let request = json!({"RemoveCuration": title});
-        send::<bool>(&request, target, 30).await
-    }
-    
-    /// Generated stub for `get-curation` remote RPC call
-    pub async fn get_curation_remote_rpc(target: &Address, title: String) -> SendResult<Option<String>> {
-        let request = json!({"GetCuration": title});
-        send::<Option<String>>(&request, target, 30).await
-    }
-    
-    /// Generated stub for `temp` remote RPC call
-    pub async fn temp_remote_rpc(target: &Address) -> SendResult<f32> {
-        let request = json!({"Temp" : {}});
-        send::<f32>(&request, target, 30).await
     }
     
     
